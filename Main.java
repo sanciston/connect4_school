97e320a07b169969c0d47eccd602771f53f922ee
sanciston <sanciston@gmail.com>
2025-04-09 12:22:36 +1100

    Commented Main.java

--- Main.java
+++ Main.java
@@ -13,32 +13,38 @@ import java.util.Scanner;
 
 
 public class Main {
+
     public static void main(String[] args) {    
         
-        System.out.print("\u000C");
+        System.out.print("\u000C"); //Clears the screnn
         
         Tiles tiles = new Tiles( //generates the board
             getIntFromIn("Enter the board width: (7 is recommended) ", 4, 20),
             getIntFromIn("Enter the board height: (6 is recommended) ", 4, 20)); 
             
+        final int streak = getIntFromIn("How many tiles in a row should the player have to get to win: (4 is recommended) ", 2, tiles.width); 
+            
         System.out.print("\u000C");
 
         
-        int boardSize = 2;    
         
         TileType nextTurn = TileType.O;
         TileType turn = TileType.O;
+
+        final int boardSize = 2; //The total size of the board, has nothing to do with tiles
+
         
         int dropX;
         
+        //Main loop
         while(true) {
             System.out.println(tiles.generateBoardHeader(boardSize * 2, boardSize) + tiles.generateBoardString(boardSize * 2, boardSize)); //Generate the board visual and print it 
-            turn = nextTurn;
+            turn = nextTurn; //Sets what tile will be placed
             
             if(turn == TileType.O) {
                 System.out.println("O Turn");
-                dropX = getIntFromIn("Where do you want to drop the token?", 1, tiles.width);
-                nextTurn = TileType.X;
+                dropX = getIntFromIn("Where do you want to drop the token?", 1, tiles.width); 
+                nextTurn = TileType.X; //Sets what tile will be placed next turn
             } else {
                 System.out.println("X Turn");
                 dropX = getIntFromIn("Where do you want to drop the token?", 1, tiles.width);
@@ -48,11 +54,11 @@ public class Main {
             
             System.out.print("\u000C");
             
-            if(!tiles.dropTile(dropX, turn)) {
+            if(!tiles.dropTile(dropX, turn)) { //Drop tile drops the tile. It returns true if the placement was valid and false if it was invalid.
                 System.out.println("Invalid placement");
             } else {
-                if(tiles.hasWon(dropX - 1, tiles.dropY - 1, 4, turn)) {
-                    if(turn == TileType.X) {
+                if(tiles.hasWon(dropX - 1, tiles.dropY - 1, streak, turn)) { //Has won checks if the game has been won
+                    if(turn == TileType.X) { //We know that whoever placed the last tile would have been the winner 
                         System.out.println("X WON");     
                         break;
                     } else {
@@ -68,24 +74,25 @@ public class Main {
 
     }
     
+    //Gets an int from user input or System.in
     public static int getIntFromIn(String print, int minimum, int maximum) {
         String errors; 
         
-        boolean hasEnteredNumber = false;
+        boolean hasEnteredNumber = false; //Has entered number will only be true once the player has entered a proper valid number
         
-        int n = 0;
+        int n = 0; //The number taken from the console
         
         while(!hasEnteredNumber) {
             
-            boolean wasSuccessful = true;
+            boolean wasSuccessful = true; //Reads wheter the string is a valid number
             
-            Scanner reader = new Scanner(System.in); // Reading from System.in
+            Scanner reader = new Scanner(System.in); // Reading from System.in (The console)
 
-            System.out.println(print);
+            System.out.println(print); //Prints out the question
         
-            try {
-                n = reader.nextInt(); 
-            } catch(Exception e) {
+            try { //Try and read the string as an int
+                n = reader.nextInt();  
+            } catch(Exception e) { //If not, catch the error and ask again
                 System.out.println("Not a number");
                 hasEnteredNumber = false;
                 wasSuccessful = false;
@@ -107,9 +114,9 @@ public class Main {
                 hasEnteredNumber = true;
             }
             
-            reader.close();
+            reader.close(); //CLose the reader
         }
         
-        return n;
+        return n; //returns n
     }
 }
